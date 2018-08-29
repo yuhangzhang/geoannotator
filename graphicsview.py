0dbcf68b1a5e41b1ef1ea299bab2b30f8c4a1e7b
Yuhang Zhang <yz1149@vdi-n2.novalocal>
2018-08-29 17:34:41 +1000

    can use right mouse key to drag the image

--- graphicsview.py
+++ graphicsview.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from PyQt5.QtWidgets import QGraphicsView
 from PyQt5.QtCore import QTimeLine
-
+from PyQt5.QtCore import Qt
 
 import PyQt5.QtCore as QtCore
 
@@ -10,9 +10,32 @@ class GraphicsView(QGraphicsView):
     def __init__(self, scene):
         super(GraphicsView,self).__init__(scene)
         self.scheduledscaling = 0
+        self.drag_switch = False
 
     #def scale(self, sx, sy):
     #    super(GraphicsView,self).scale(sx, sy)
+
+    def mousePressEvent(self, event):
+        super(GraphicsView, self).mousePressEvent(event)
+        if event.button() == Qt.RightButton:
+            self.drag_switch = True
+            self.lastpos=event.pos()
+        else:
+            self.lastpos=None
+
+    def mouseMoveEvent(self, event):
+        super(GraphicsView, self).mouseMoveEvent(event)
+        if self.drag_switch == True:
+            offset = event.pos()-self.lastpos
+            self.horizontalScrollBar().setValue(self.horizontalScrollBar().value()+ offset.x())
+            self.verticalScrollBar().setValue(self.verticalScrollBar().value() + offset.y())
+            self.show()
+            self.lastpos=event.pos()
+
+    def mouseReleaseEvent(self, event):
+        super(GraphicsView, self).mouseReleaseEvent(event)
+        if event.button() == Qt.RightButton:
+            self.drag_switch = False
 
 
     def wheelEvent(self, event):
